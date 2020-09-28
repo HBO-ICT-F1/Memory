c15e4de2684ad9e0123813755f23ba8158143278
TheDutchScorpion <32477921+TheDutchScorpion@users.noreply.github.com>
2020-09-28 14:29:48 +0200

    Add loading page from MainWindow

--- ui/pages/MainPage.xaml.cs
+++ ui/pages/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Memory.ui.pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Memory.ui
+namespace Memory.ui.pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-            _UIFrame.Content = new MainPage();
         }
     }
 }
