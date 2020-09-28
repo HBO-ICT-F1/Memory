d00e7bf06a57ef6fd0a464ae0c48546ee9760b32
TheDutchScorpion <32477921+TheDutchScorpion@users.noreply.github.com>
2020-09-28 14:07:20 +0200

    Add ui folder and move MainWindow to ui folder.

--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Memory
+namespace Memory.ui
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
