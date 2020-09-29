09b28c12c316578d516be5adf7cedd6bfcd9c3cb
LunkLoafGrumble <rutgerminecraft@live.nl>
2020-09-29 15:17:09 +0200

    Added page C#

---
+++ ui/pages/ScoreboardPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Memory.ui.pages
+{
+    public partial class ScoreboardPage : Page
+    {
+        public ScoreboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
