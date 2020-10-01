d9ece5b596057721d7566393358fb353bd2a4b7e
TheDutchScorpion <32477921+TheDutchScorpion@users.noreply.github.com>
2020-10-01 10:40:56 +0200

    Add drawing buttons in game grid Add default theme images

--- ui/pages/GamePage.xaml.cs
+++ ui/pages/GamePage.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Memory.card;
+using Path = System.IO.Path;
 
 namespace Memory.ui.pages
 {
@@ -20,17 +15,70 @@ namespace Memory.ui.pages
     /// </summary>
     public partial class GamePage : Page
     {
+        public double cardScaleHeight = 2;
+        public double cardScaleWidth = 1.5;
+        
         public GamePage()
         {
             InitializeComponent();
-            generateCards();
+            
+            ShowCards();
         }
 
-        private void generateCards()
+        private void ShowCards()
         {
-            Button button = new Button();
-            button.Content = "123";
-            _CardBox.Children.Add(button);
+            string[] images = Directory.GetFiles($"{Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))}/ui/assets/themes/default", "*");
+            List<Card> cards = Card.Generate(images);
+
+            double rows = Math.Sqrt(cards.Count);
+            double columns = Math.Sqrt(cards.Count);
+            double maxScale = Math.Min(SystemParameters.PrimaryScreenHeight / rows, SystemParameters.PrimaryScreenWidth / columns);
+            double maxScaleSize = Math.Max(cardScaleHeight, cardScaleWidth);
+            int cardHeight = (int)(cardScaleHeight * maxScale / maxScaleSize);
+            int cardWidth = (int)(cardScaleWidth * maxScale / maxScaleSize);
+            
+            Grid Grid = new Grid();
+            Grid.Width = cardWidth * columns;
+            Grid.HorizontalAlignment = HorizontalAlignment.Center;
+            Grid.VerticalAlignment = VerticalAlignment.Center;
+
+            for (int i = 0; i < columns; i++)
+            {
+                ColumnDefinition Column = new ColumnDefinition();
+                Grid.ColumnDefinitions.Add(Column);
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                RowDefinition Row = new RowDefinition();
+                Row.Height = new GridLength(cardHeight);
+                Grid.RowDefinitions.Add(Row);
+            }
+
+            int index = 0;
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    Button button = new Button();
+                    Card card = cards[index];
+                    button.Click += (sender, e) => {
+                        Button currentButton = sender as Button;
+                        ButtonHandler(card, currentButton);
+                    };
+                    Grid.SetRow(button, y);
+                    Grid.SetColumn(button, x);
+                    Grid.Children.Add(button);
+                    index++;
+                }
+            }
+            CardBox.Children.Add(Grid);
+        }
+
+        private void ButtonHandler(Card card, Button button)
+        {
+            // sets.Add(button);
+            Debug.WriteLine("test");
+            button.Background = new ImageBrush(card.Image);
         }
     }
 }
