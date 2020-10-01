4e7f56a48ed4ac16e325c0291b0ca336b55d78bf
LunkLoafGrumble <rutgerminecraft@live.nl>
2020-10-01 11:22:32 +0200

    Added escape menu page files

---
+++ ui/pages/EscapeMenuPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Memory.ui.pages
+{
+    public partial class EscapeMenuPage : Page
+    {
+        public EscapeMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Continue(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var main = MainWindow.GetMainWindow();
+            main.escapeMenuToggle = !main.escapeMenuToggle;
+            main.DrawEscapeMenu();
+        }
+
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.QuitApplication();
+        }
+    }
+}
