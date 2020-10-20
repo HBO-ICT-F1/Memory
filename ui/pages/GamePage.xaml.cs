b2524a8a8d0c67749660e2371e196b780fc39bf6
Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>
2020-10-20 12:05:18 +0200

    Revert "Computer agent"

--- ui/pages/GamePage.xaml.cs
+++ ui/pages/GamePage.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Memory.card;
@@ -17,15 +15,12 @@ namespace Memory.ui.pages
     /// </summary>
     public partial class GamePage : Page
     {
-        private readonly List<Card> cards = new List<Card>();
-        public Dictionary<int, int> shownCards = new Dictionary<int, int>();
-        public Dictionary<int, Image> cardImages = new Dictionary<int, Image>();
-        public double cardScaleHeight = 2;
-        public double cardScaleWidth = 2;
+        public const double CardScaleHeight = 2;
+        public const double CardScaleWidth = 2;
+        private readonly List<Card> _cards;
         public Grid grid = new Grid();
         public Dictionary<int, Image> images = new Dictionary<int, Image>();
         public List<int> selectedCards = new List<int>();
-        public Uri defaultCardImage;
 
         public GamePage()
         {
@@ -35,9 +30,6 @@ namespace Memory.ui.pages
                 Directory.GetFiles(
                     $"{Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))}/ui/assets/themes/default/cards",
                     "*");
-            defaultCardImage =
-                new Uri(
-                    $"{Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))}/ui/assets/themes/default/default.jpg");
 
             _cards = Card.Generate(images);
             ShowCards();
@@ -90,8 +82,9 @@ namespace Memory.ui.pages
                 };
 
                 image.Source =
-                    new BitmapImage(defaultCardImage);
-                cardImages.Add(index, image);
+                    new BitmapImage(new Uri(
+                        $"{Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))}/ui/assets/themes/default/default.jpg"));
+                images.Add(index, image);
                 Grid.SetRow(image, y);
                 Grid.SetColumn(image, x);
 
@@ -108,11 +101,6 @@ namespace Memory.ui.pages
             cardImage.Source = card.image;
             selectedCards.Add(index);
 
-            if (!shownCards.ContainsKey(index))
-            {
-                shownCards.Add(index, card.Type);
-            }
-
             if (selectedCards.Count < 2) return;
             await CheckCards();
             selectedCards.Clear();
@@ -127,125 +115,15 @@ namespace Memory.ui.pages
                 grid.Children.Remove(images[selectedCards[0]]);
                 images.Remove(selectedCards[0]);
 
-                grid.Children.Remove(cardImages[selectedCards[1]]);
-                cardImages.Remove(selectedCards[1]);
-
-                shownCards.Remove(selectedCards[0]);
-                shownCards.Remove(selectedCards[1]);
+                grid.Children.Remove(images[selectedCards[1]]);
+                images.Remove(selectedCards[1]);
                 return;
             }
 
             await Task.Delay(1000);
-            cardImages[selectedCards[0]].Source = cardImages[selectedCards[1]].Source =
-                new BitmapImage(defaultCardImage);
-            await ComputerAgent();
-        }
-
-        private async Task ComputerAgent()
-        {
-            var typeCount = new Dictionary<int, List<int>>();
-            foreach (var card in shownCards)
-            {
-                if (!typeCount.ContainsKey(card.Value))
-                {
-                    var cards = new List<int>();
-                    cards.Add(card.Key);
-                    typeCount.Add(card.Value, cards);
-                }
-                else if (!typeCount[card.Value].Contains(card.Key))
-                {
-                    typeCount[card.Value].Add(card.Key);
-                }
-            }
-
-            int? typeIndex = null;
-            foreach (var type in typeCount)
-            {
-                if (type.Value.Count == 2)
-                {
-                    typeIndex = type.Key;
-                    break;
-                }
-            }
-
-            int cardOne;
-            int cardTwo;
-            if (typeIndex == null)
-            {
-                cardOne = await PickRandomKey();
-                cardTwo = await PickRandomKey(cardOne);
-
-                if (!shownCards.ContainsKey(cardOne))
-                {
-                    shownCards.Add(cardOne, cards[cardOne].Type);
-                }
-
-                if (!shownCards.ContainsKey(cardTwo))
-                {
-                    shownCards.Add(cardTwo, cards[cardTwo].Type);
-                }
-
-                await Task.Delay(1000);
-                cardImages[cardOne].Source = cards[cardOne].Image;
-                await Task.Delay(1000);
-                cardImages[cardTwo].Source = cards[cardTwo].Image;
-                await Task.Delay(1000);
-                if (cards[cardOne].Type != cards[cardTwo].Type)
-                {
-                    cardImages[cardOne].Source = cardImages[cardTwo].Source =
-                        new BitmapImage(defaultCardImage);
-                    return;
-                }
-            }
-            else
-            {
-                cardOne = typeCount[(int) typeIndex][0];
-                cardTwo = typeCount[(int) typeIndex][1];
-                await Task.Delay(1000);
-                cardImages[typeCount[(int) typeIndex][0]].Source = cards[typeCount[(int) typeIndex][0]].Image;
-                await Task.Delay(1000);
-                cardImages[typeCount[(int) typeIndex][1]].Source = cards[typeCount[(int) typeIndex][1]].Image;
-                await Task.Delay(500);
-            }
-
-            // TODO: increment score
-
-            if (shownCards.ContainsKey(cardOne))
-            {
-                shownCards.Remove(cardOne);
-            }
-
-            if (shownCards.ContainsKey(cardTwo))
-            {
-                shownCards.Remove(cardTwo);
-            }
-
-            grid.Children.Remove(cardImages[cardOne]);
-            cardImages.Remove(cardOne);
-
-            grid.Children.Remove(cardImages[cardTwo]);
-            cardImages.Remove(cardTwo);
-
-            shownCards.Remove(cardOne);
-            shownCards.Remove(cardTwo);
-            if (grid.Children.Count > 0)
-            {
-                await ComputerAgent();
-            }
-        }
-
-        private async Task<int> PickRandomKey(int? retryKey = null)
-        {
-            Random rand = new Random();
-            var keyList = new List<int>(cardImages.Keys);
-            int key = keyList[rand.Next(keyList.Count)];
-            if (key == retryKey)
-            {
-                await Task.Delay(10);
-                return await PickRandomKey(retryKey);
-            }
-
-            return key;
+            images[selectedCards[0]].Source = images[selectedCards[1]].Source =
+                new BitmapImage(new Uri(
+                    $"{Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()))}/ui/assets/themes/default/default.jpg"));
         }
     }
 }
