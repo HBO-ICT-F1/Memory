322a8063ac40f46fad2f02c7594c119dcdc08e26
Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>
2020-10-30 12:02:05 +0100

    Scoreboard finished
    Signed-off-by: Rutger <32477913+LunkLoafGrumble@users.noreply.github.com>

--- ui/pages/ScoreboardPage.xaml.cs
+++ ui/pages/ScoreboardPage.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System.Windows.Controls;
-using System.Collections.Generic;
-using System;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Memory.ui.pages
 {
@@ -10,35 +10,24 @@ namespace Memory.ui.pages
         public ScoreboardPage()
         {
             InitializeComponent();
-            var scores = new List<Tuple<int, string, int>>();
-            scores.Add(new Tuple<int, string, int>(1, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(2, "Robert", 14));
-            scores.Add(new Tuple<int, string, int>(3, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(4, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(5, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(6, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(1, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(7, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(8, "Jesse", 15));
-            scores.Add(new Tuple<int, string, int>(9, "Jesse", 15));
-
-            foreach (var item in scores)
+            for (var i = 1; i < 11; i++)
             {
-                var group = new TableRowGroup();
                 var row = new TableRow();
-                var positionCell = AddCell(item.Item1);
-                var nameCell = AddCell(item.Item2);
-                var scoreCell = AddCell(item.Item3);                
+                for (var j = 1; j < 4; j++)
+                {
+                    var cell = new TableCell();
+                    cell.Blocks.Add(new Paragraph(new Run("Database top " + i)));
+                    cell.Background = Brushes.Orange;
+                    row.Cells.Add(cell);
+                }
+
+                table.Rows.Add(row);
             }
         }
 
-        private TableCell AddCell(object content)
+        private void Back(object sender, RoutedEventArgs e)
         {
-            var cell = new TableCell();
-            var run = new Run(content.ToString());
-            var paragraph = new Paragraph(run);
-            cell.Blocks.Add(paragraph);
-            return cell;
+            MainWindow.GetMainWindow().ChangePage(new MainPage());
         }
-    }   
+    }
 }
